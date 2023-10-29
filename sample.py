737940fb96d52d9b765bf98e65a2bdaa6bfd7f9c
abeeryounas <abeeryounas43@gmail.com>
2023-10-29 17:55:59 +0500

    day 3

--- sample.py
+++ sample.py
@@ -2,6 +2,10 @@ print("Fruits is goods for health")
 for abeer in range(10):
  print(abeer)
 
- print("Fruits is goods for health")
+ print("for second loop")
+for abeer in range(10):
+ print(abeer)
+
+ print("For using third loop")
 for abeer in range(10):
  print(abeer)
